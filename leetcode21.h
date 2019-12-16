d25400ad5e6dd4d31212f0fce2bfb9cd4f004ca2
zhaobin <18260068839@163.com>
2019-12-16 09:43:57 +0800

    leetcode21完成

--- leetcode21.h
+++ leetcode21.h
@@ -22,15 +22,29 @@ public:
 	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
 		ListNode* p1 = l1;
 		ListNode* p2 = l2;
-		ListNode* p = new ListNode(NULL);
-		while (p1 && p2)
+		ListNode* result = new ListNode(-1);  //注意初始化的方法
+		ListNode* p = result;
+		while (p1 != nullptr && p2 != nullptr)  //这里空指针使用nullptr！！！！
 		{
 			if (p1->val > p2->val)
 			{
-
+				p->next = p2;
+				p2 = p2->next;
 			}
+			else
+			{
+				p->next = p1;
+				p1 = p1->next;
+			}
+			p = p->next;
 
 		}
+		if (p1 != nullptr)
+			p->next = p1;
+		else if (p2 != nullptr)  //这里空指针使用nullptr！！！！
+			p->next = p2;
+
+		return result->next;
 
 
 	}
