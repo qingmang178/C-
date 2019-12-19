56017be3559a9feecafbd31ca64571641c63201c
zhaobin <18260068839@163.com>
2019-12-19 20:48:00 +0800

    leetcode69

---
+++ leetcode69.h
@@ -0,0 +1,36 @@
+#pragma once
+#ifndef leetcode69_
+#define leetcode69_
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int mySqrt(int x) {
+		long long left = 0;
+		long long right = x / 2;
+		while (left < right)
+		{
+			long long mid = left + (right - left+1) / 2;
+			long long res = mid * mid;
+			if (res > x)
+				right = mid - 1;
+			else
+			{
+				left = mid;
+
+			}
+		}
+		return left;
+	}
+};
+
+
+
+#endif
--- test.cpp
+++ test.cpp
@@ -1,19 +1,14 @@
 #include<iostream>
-#include"leetcode67.h"
+#include"leetcode69.h"
 using namespace std;
 
 
 int main()
 {
-	string a = "1010";
-	string c = "1011";
+	int a = 8;
 	Solution solu;
-	string b = solu.addBinary(a,c);
-	for (int i = 0; i < b.size(); i++)
-		cout << b[i] << " ";
-
-
-
+	int b = solu.mySqrt(a);
+	cout << b;
 
 }
 
