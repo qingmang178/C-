52ad34244f2adc2470811107456998b156df743f
zhaobin <18260068839@163.com>
2019-12-20 19:00:53 +0800

    leetcode88完整

--- leetcode88.h
+++ leetcode88.h
@@ -13,15 +13,15 @@ using namespace std;
 class Solution {
 public:
 	void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-		for (int i = 0; i < nums2.size(); i++) 
+		for (int i = 0; i < nums2.size(); i++)
 		{
-			nums1[m+i]=nums2[i];
+			nums1[m + i] = nums2[i];
 		}
 		sort(nums1.begin(), nums1.end());
 
-};
+	};
 
-
+}
 
 
 
