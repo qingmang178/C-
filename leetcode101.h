d04bbaa0c863a4937e388a030bc29f58aa85b358
zhaobin <18260068839@163.com>
2019-12-20 19:50:39 +0800

    leetcode101对称二叉树 队列迭代法

--- leetcode101.h
+++ leetcode101.h
@@ -8,6 +8,7 @@
 #include<vector>
 #include<string>
 #include<algorithm>
+#include<queue>
 
 using namespace std;
 
@@ -25,7 +26,7 @@ struct TreeNode {
 
 class Solution {
 public:
-	bool isSymmetric(TreeNode* root) {
+	/*bool isSymmetric(TreeNode* root) {
 
 		return isMirror(root, root);
 	}
@@ -36,6 +37,48 @@ public:
 		if (t1 == NULL || t2 == NULL)
 			return false;
 		return (t1->val == t2->val) && isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
+	}*/
+	bool isSymmetric(TreeNode* root)
+	{
+		queue<TreeNode*> q;
+		q.push(root);
+		q.push(root);
+		while (!q.empty())
+		{
+			TreeNode* t1 = q.front();
+			q.pop();
+			TreeNode* t2 = q.front();
+			q.pop();
+			if (t1 == NULL && t2 == NULL)
+				continue;
+			if (t1 == NULL || t2 == NULL)
+				return false;
+			if (t1->val != t2->val)
+				return false;
+			q.push(t1->left);
+			q.push(t2->right);
+			q.push(t1->right);
+			q.push(t2->left);
+		}
+		return true;
+
+		/*  java代码非递归实现
+		Queue<TreeNode> q = new LinkedList<>();
+	q.add(root);
+	q.add(root);
+	while (!q.isEmpty()) {
+		TreeNode t1 = q.poll();
+		TreeNode t2 = q.poll();
+		if (t1 == null && t2 == null) continue;
+		if (t1 == null || t2 == null) return false;
+		if (t1.val != t2.val) return false;
+		q.add(t1.left);
+		q.add(t2.right);
+		q.add(t1.right);
+		q.add(t2.left);
+	}
+	return true;
+		*/
 	}
 };
 
