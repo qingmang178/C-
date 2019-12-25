e8828ef28d70f081a43192ac38b5a34f707781c2
zhaobin <18260068839@163.com>
2019-12-25 15:24:50 +0800

    leetcode125模板

---
+++ leetcode125.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef leetcode125_
+#define leetcode125_
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include <utility>
+
+
+using namespace std;
+
+class Solution {
+public:
+
+
+	bool isPalindrome(string s) {
+
+	}
+};
+
+
+
+#endif
