e879904ee0f7b7348e592c6d7bd21fa391b5de53
zhaobin <18260068839@163.com>
2020-01-03 10:02:45 +0800

    leetcode237

---
+++ leetcode237.h
@@ -0,0 +1,38 @@
+#pragma once
+#pragma once
+#ifndef leetcode237_
+#define leetcode237_
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include <utility>
+#include <map>
+#include<unordered_set>
+
+
+
+using namespace std;
+//定义一个单链表
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+class Solution {
+public:
+	void deleteNode(ListNode* node) {
+		node->val = node->next->val;
+		node->next = node->next->next;
+	}
+};
+
+
+#endif
+
