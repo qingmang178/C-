bbb79579e9c64c3053e1d57df7956943ce9659af
zhaobin <18260068839@163.com>
2020-01-06 15:41:39 +0800

    leetcode326

---
+++ leetcode326.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef leetcode299_
+#define leetcode299_
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include <utility>
+#include <map>
+#include<unordered_set>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+	bool isPowerOfThree(int n) {
+		/*if (n == 0)
+			return false;
+		while (n > 1)
+		{
+			if (n % 3 != 0)
+				return false;
+			n = n / 3;
+		}
+		return n == 1;*/
+
+
+	}
+};
+
+
+
+
+#endif
