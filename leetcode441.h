6b31b63ea580897c91b7a86dae4e5e5a4e0e57af
zhaobin <18260068839@163.com>
2020-01-16 10:21:11 +0800

    leetcode441

--- leetcode441.h
+++ leetcode441.h
@@ -1,1 +1,34 @@
 #pragma once
+#ifndef leetcode0000_
+#define leetcode0000_
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<queue>
+#include<stack>
+#include <utility>
+#include <map>
+#include<unordered_set>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+	int arrangeCoins(int n) {
+		int i = 0;
+		while (1)
+		{
+			n = n - i;
+			if (n < i + 1)
+				break;
+			i++;
+		}
+		return i;
+	}
+};
+
+#endif
